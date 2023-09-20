59406002e2750b0cad950757538a81d87f03cfe8
Gusttavo-costta <gccostta23@gmail.com>
2023-09-20 00:05:17 -0300

    React Router

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom"
+import styles from './Navbar.module.css'
 
 function NavBar() {
     return (
         <>
-            <ul>
-                <li>
+            <ul className={styles.list}> 
+                <li className={styles.item}>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link to="/empresa">Empresa</Link>
                 </li>
-                <li>
+                <li className={styles.item}>
                     <Link to="/contato">Contato</Link>
                 </li>
             </ul>
